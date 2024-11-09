6254a9e0d44bd0f2e7d22e178fbb9a56b4500441
robyseelps <31707091+robyseelps@users.noreply.github.com>
2024-11-09 10:55:02 +0100

    Update and rename webhook_v1.py to webhooks/dflow_gemini_webhook.py
    changed webhook prompt

--- webhooks/dflow_gemini_webhook.py
+++ webhooks/dflow_gemini_webhook.py
@@ -13,8 +13,14 @@ def webhook(request):
 
     user_query = req['queryResult']['queryText']
 
+    custom_prompt = ("Imagine you are a financial hygiene tutor. "
+                     "Try to answer your student in a compact, short, "
+                     "and highly informative way. write answer as one plaintext string do not use * characters, it's very important. Answer the following question: ")
+    full_prompt = custom_prompt + user_query
+
+
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(user_query)
+    response = model.generate_content(full_prompt)
 
     return jsonify({
         "fulfillmentMessages": [
